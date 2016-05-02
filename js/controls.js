510524b0530d0a40b24d97ec581b86159938a770
Lawrence X. Rogers <lawrence.x.rogers@gmail.com>
2016-05-01 17:38:00 -0700

    fixed merge conflict

--- js/controls.js
+++ js/controls.js
@@ -1,10 +1,6 @@
 $('.DOW-checkbox').each(function() {
-<<<<<<< HEAD
 	$(this).change(updateDOW)
 	$(this).prop('checked', true)
-=======
-	$(this).change(updateDOW);
-	$(this).prop('checked', true);
 });
 
 $('.location-checkbox').each(function() {
@@ -13,5 +9,4 @@ $('.location-checkbox').each(function() {
     if (this.name === 'Work') filter_work = !filter_work;
     updateDynamicFilter();
   });
->>>>>>> 4e1d3379d6ffc19774f5773eb3e917c76ee7611e
 });
